7dd6d62e0e4ab28943ad3f4f7d8f437893930b30
saritha <137024788+saritha-00@users.noreply.github.com>
2023-10-10 23:05:02 -0400

    Create nicejob.js

---
+++ nicejob/nicejob.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/nicejob', (req, res) => {
+  res.send('Nice Job!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
