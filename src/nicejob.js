487397f73c77eac43df8c68c7f7606222fc8f01d
saritha <137024788+saritha-00@users.noreply.github.com>
2023-10-10 22:23:35 -0400

    Update nicejob.js

--- src/nicejob.js
+++ src/nicejob.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = 3000
 
-app.get('/helloworld', (req, res) => {
-  res.send('Hello World!')
+app.get('/nicejob', (req, res) => {
+  res.send('Nice Job!')
 })
 
 app.listen(port, () => {
